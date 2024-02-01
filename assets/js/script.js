7e5830e72392bb6328f87323493abfa0be5885fc
RinaldyIqbal <158501388+RinaldyIqbal@users.noreply.github.com>
2024-02-02 01:34:38 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,18 +56,6 @@ $(document).ready(function () {
 
 });
 
-document.addEventListener('visibilitychange',
-    function () {
-        if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Rinaldy Iqbal";
-            $("#favicon").attr("href", "assets/images/favicon.png");
-        }
-        else {
-            document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favhand.png");
-        }
-    });
-
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
